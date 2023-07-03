233fa550b933d39140755e95b58625fe4bee9218
Stanislav Alexandrov <staskasnz@mail.ru>
2023-07-03 11:37:05 +0300

    env and port3000

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const { DB_ADDRESS } = require('./config');
 //   'https://api.staskasnz.nomoreparties.sbs',
 // ];
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(cors());
